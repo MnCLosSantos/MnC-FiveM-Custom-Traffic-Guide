81e0e0abc02e6ac9ce4d4aa78bf45cefeaa46933
MnCLosSantos <ccpautomotivegroup@gmail.com>
2025-02-20 00:53:01 +0000

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+------- STAN THE FUCKIN MAN -------
+
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Stan Leigh'
+description 'a custom traffic mod'
+version '1.0.0'
+
+data_file 'FIVEM_LOVES_YOU_341B23A2F0E0F131' 'popgroups.ymt'
+
+files {
+    'popgroups.ymt',
+}
+
+
+lua54 'yes'
